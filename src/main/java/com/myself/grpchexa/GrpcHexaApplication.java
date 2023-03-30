c6d748314d828674ef2a2607fab424afac391e4a
Steve Alves-Blyt <steve.alves@protonmail.com>
2023-03-30 15:52:38 +0200

    chore: init repo

---
+++ src/main/java/com/myself/grpchexa/GrpcHexaApplication.java
@@ -0,0 +1,13 @@
+package com.myself.grpchexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcHexaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcHexaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myself/grpchexa/GrpcHexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myself.grpchexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcHexaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
